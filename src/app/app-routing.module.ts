85593b8bcf83a7be1d3afea85d960c2d38a0b370
unknown <sharadrayabav@Next-Hyd-293.nexteducation.com>
2018-02-16 13:39:32 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: HomeComponent
+  },{
+    path:'about',
+    component: AboutComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: []
+  exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  itemCount:number;
+  btnText:string='Add item';
+  btnRemove:string='Remove Item';
+  goalText:string='My first goal';
+  goals=[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount=this.goals.length;
+  }
+addItem(){
+  this.goals.push(this.goalText);
+  this.goalText='';
+  this.itemCount=this.goals.length;
+}
+removeItem(i){
+  this.goals.splice(i,1);
+
+}
+}
